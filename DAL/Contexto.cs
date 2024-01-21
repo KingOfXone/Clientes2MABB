582e7211bfdc338753e85c01d6dc8cd948555647
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-01-20 21:19:47 -0400

    Agregando el Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ClientesMABB.DAL
+{
+    public class Contexto:DbContext
+    {
+        public DbSet<Clientes> Clientes { get; set; }
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    }
+}
